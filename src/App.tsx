62807629d016d1337348b46e85465d69ea0e602d
Erick Fernandes <erickfb56@gmail.com>
2022-04-04 10:39:08 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState } from "react";
+import { QUOTES_ARRAY, randQuote, randAuthor } from "./quotesArray";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const hexColor = ():string => {
+    const randHexColor: string = (Math.random() * 0xfffff * 1000000).toString(16);
+    return `#${randHexColor.slice(0, 6)}`;
+  };
+  const [quote, setQuote] = useState<String>(randQuote);
+  const [author, setAuthor] = useState<String>(randAuthor);
+  const [mainColor, setMainColor] = useState<string>(hexColor());
+
+  const getQuote = ():void => {
+    const randIndex: number = Math.floor(QUOTES_ARRAY.length * Math.random());
+    setMainColor(hexColor());
+    setQuote(QUOTES_ARRAY[randIndex].quote);
+    setAuthor(QUOTES_ARRAY[randIndex].author);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header className="App-header"
+      style={{ backgroundColor: mainColor, color: mainColor }}>
+        
+        <div className="quote-box">    
+          <h3 id="quote">            
+            {`"${quote}"`}
+            <br />
+          </h3>
+          <p id="author"> {`—  ${author}`} </p>
+          <div className="buttons">
+            
+            <a
+              id="tweet-quote"
+              href={encodeURI(
+                `https://twitter.com/intent/tweet?text="${quote}" —  ${author}`
+              )}
+              style={{ color: "#1DA1F2" }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <button
+              id="new-quote"
+              onClick={getQuote}
+              style={{ backgroundColor: mainColor }}
+            >
+              
+              Get Random Quote
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/quotesArray.tsx
@@ -0,0 +1,401 @@
+interface IQuotes{
+  quote: String,
+  author: String
+  
+}
+
+export const QUOTES_ARRAY:IQuotes[] = [
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote: "So many books, so little time.",
+    author: "Frank Zappa",
+  },
+  {
+    quote: "A room without books is like a body without a soul.",
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    quote: "A friend is someone who knows all about you and still loves you.",
+    author: "Elbert Hubbard",
+  },
+  {
+    quote: "Without music, life would be a mistake.",
+    author: "Friedrich Nietzsche",
+  },
+  {
+    quote:
+      "It is better to be hated for what you are than to be loved for what you are not.",
+    author: "Andre Gide",
+  },
+  {
+    quote: `Nothing is impossible. The word itself says 'I'm possible!'`,
+    author: "Audrey Hepburn",
+  },
+  {
+    quote: "There is nothing impossible to they who will try.",
+    author: "Alexander the Great",
+  },
+  {
+    quote: "The bad news is time flies. The good news is you’re the pilot.",
+    author: "Michael Altshuler",
+  },
+  {
+    quote:
+      "Life has got all those twists and turns. You’ve got to hold on tight and off you go.",
+    author: "Nicole Kidman",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote:
+      "You define your own life. Don’t let other people write your script.",
+    author: "Oprah Winfrey",
+  },
+  {
+    quote:
+      "Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on.",
+    author: "Lady Gaga",
+  },
+  {
+    quote:
+      "You don’t always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.",
+    author: "Mandy Hale",
+  },
+  {
+    quote:
+      "I’m going to be gone one day, and I have to accept that tomorrow isn’t promised. Am I OK with how I’m living today? It’s the only thing I can help. If I didn’t have another one, what have I done with all my todays? Am I doing a good job?",
+    author: "Hayley Williams",
+  },
+  {
+    quote:
+      "Not having the best situation, but seeing the best in your situation is the key to happiness.",
+    author: "Marie Forleo",
+  },
+  {
+    quote: "Believe you can and you’re halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "Just don’t give up trying to do what you really want to do. Where there is love and inspiration, I don’t think you can go wrong.",
+    author: "Ella Fitzgerald",
+  },
+  {
+    quote:
+      "Everyone has inside of him a piece of good news. The good news is that you don’t know how great you can be! How much you can love! What you can accomplish! And what your potential is!",
+    author: "Anne Frank",
+  },
+  {
+    quote:
+      "All you need is the plan, the road map, and the courage to press on to your destination.",
+    author: "Earl Nightingale",
+  },
+  {
+    quote:
+      "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
+    author: "Roald Dahl",
+  },
+  {
+    quote: "Try to be a rainbow in someone’s cloud.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "We must let go of the life we have planned, so as to accept the one that is waiting for us.",
+    author: "Joseph Campbell",
+  },
+  {
+    quote: "Wake up determined, go to bed satisfied.",
+    author: "Dwayne “The Rock” Johnson",
+  },
+
+  {
+    quote:
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    author: "Benjamin Franklin",
+  },
+
+  {
+    quote:
+      "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+    author: "Helen Keller",
+  },
+
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+
+  {
+    quote: `Many of life's failures are people who did not realize how close they were to success when they gave up.`,
+    author: `Thomas A. Edison`,
+  },
+
+  {
+    quote: `Only a life lived for others is a life worthwhile.`,
+    author: `Albert Einstein`,
+  },
+
+  {
+    quote: `Do not let making a living prevent you from making a life.`,
+    author: `John Wooden`,
+  },
+
+  {
+    quote: `Keep smiling, because life is a beautiful thing and there's so much to smile about.`,
+    author: `Marilyn Monroe`,
+  },
+
+  {
+    quote: `In three words I can sum up everything I've learned about life: it goes on.`,
+    author: `Robert Frost
+    `,
+  },
+
+  {
+    quote: `Life is made of ever so many partings welded together.`,
+    author: `Charles Dickens`,
+  },
+
+  {
+    quote: `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma — which is living with the results of other people's thinking.`,
+    author: `Steve Jobs`,
+  },
+
+  {
+    quote: `Life is trying things to see if they work.`,
+    author: `Ray Bradbury`,
+  },
+
+  {
+    quote: `You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.`,
+    author: `Dr. Seuss`,
+  },
+
+  {
+    quote: `A successful marriage requires falling in love many times, always with the same person.`,
+    author: `Mignon McLaughlin`,
+  },
+
+  {
+    quote: `What is Love? I have met in the streets a very poor young man who was in love. His hat was old, his coat worn, the water passed through his shoes and the stars through his soul.`,
+    author: `Victor Hugo`,
+  },
+
+  {
+    quote: `Hard work always pays off, whatever you do.`,
+    author: `Dustin Lynch`,
+  },
+
+  {
+    quote: `A lot of hard work is hidden behind nice things.`,
+    author: `Ralph Lauren`,
+  },
+
+  {
+    quote: `Preparation for tomorrow is hard work today.`,
+    author: `Bruce Lee`,
+  },
+
+  {
+    quote: `Success is nothing more than a few simple disciplines, practiced every day.`,
+    author: `Jim Rohn`,
+  },
+  {
+    quote: `It’s not about money or connections – it’s the willingness to outwork and outlearn everyone.`,
+    author: `Mark Cuban`,
+  },
+  {
+    quote: `I’ve found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often.`,
+    author: `Brian Tracy`,
+  },
+  {
+    quote: `Do what you have to do until you can do what you want to do.`,
+    author: `Oprah Winfrey`,
+  },
+  {
+    quote: `Talent is given by God but you should respect it by doing good work.`,
+    author: `Johnny Lever`,
+  },
+  {
+    quote: `Chop your own wood and it will warm you twice.`,
+    author: `Henry Ford`,
+  },
+  {
+    quote: `Great things come from hard work and perseverance. No excuses.`,
+    author: `Kobe Bryant`,
+  },
+  {
+    quote: `If you do the work you get rewarded. There are no shortcuts in life.`,
+    author: `Michael Jordan`,
+  },
+  {
+    quote: `There are no shortcuts to any place worth going.`,
+    author: `Beverly Sills`,
+  },
+  {
+    quote: `It's fun to do the impossible.`,
+    author: `Walt Disney`,
+  },
+  {
+    quote: `Quality is never an accident. It is always the result of intelligent effort.`,
+    author: `John Ruskin`,
+  },
+
+  {
+    quote: `We tend to get what we expect.`,
+    author: `Norman Vincent Peale`,
+  },
+
+  {
+    quote: `Your brain is the most important organ in your body, and what happens in it determines what you think and feel, say and do.`,
+    author: `Rick Hanson`,
+  },
+
+  {
+    quote: `No one would have crossed the ocean if he could have gotten off the ship in the storm.`,
+    author: `Charles Kettering`,
+  },
+
+  {
+    quote: `The ability to influence people without irritating them is the most profitable skill you can learn.`,
+    author: `Napoleon Hill`,
+  },
+
+  {
+    quote: `The best thinking has been done in solitude. The worst has been done in turmoil.`,
+    author: `Thomas Edison`,
+  },
+
+  {
+    quote: `Happiness doesn't come from doing what we like to do but from liking what we have to do.`,
+    author: `Wilferd Peterson`,
+  },
+
+  {
+    quote: `Happy is the man who can make a living by his hobby.`,
+    author: `George Bernard Shaw`,
+  },
+
+  {
+    quote: `Unless you try to do something beyond what you have already mastered, you will never grow.`,
+    author: `Ralph Waldo Emerson`,
+  },
+
+  {
+    quote: `Questions provide the key to unlocking our unlimited potential.`,
+    author: `Anthony Robbins`,
+  },
+
+  {
+    quote: `Don't find fault, find a remedy; anybody can complain.`,
+    author: `Henry Ford`,
+  },
+  {
+    quote: `Enjoy all you have while pursuing all you want.`,
+    author: `Jim Rohn`,
+  },
+  {
+    quote: `When you are through changing, you are through.`,
+    author: `Bruce Barton`,
+  },
+  {
+    quote: `Experience has been a stern but excellent teacher.`,
+    author: `Og Mandino`,
+  },
+  {
+    quote: `Well done, is better than well said.`,
+    author: `Benjamin Franklin`,
+  },
+  {
+    quote: `It always seems impossible until it's done.`,
+    author: `Nelson Mandela`,
+  },
+  {
+    quote: `A man's true wealth is the good he does in the world.`,
+    author: `Kahlil Gibran`,
+  },
+  {
+    quote: `Only a mediocre person is always at his best.`,
+    author: `W. Somerset Maugham`,
+  },
+  {
+    quote: `Life has no limitations, except the ones you make.`,
+    author: `Les Brown`,
+  },
+  {
+    quote: `Football is like life, it requires perseverance, self-denial, hard work sacrifice, dedication and respect for authority.`,
+    author: `Vince Lombardi`,
+  },
+  {
+    quote: `As you know, life is an echo; we get what we give.`,
+    author: `David DeNotaris`,
+  },
+  {
+    quote: `I enjoy every day, because I choose to.`,
+    author: `Tony Clark`,
+  },
+  {
+    quote: `Find people who will make you better.`,
+    author: `Michelle Obama`,
+  },
+  {
+    quote: `I would rather be able to appreciate things I cannot have than to have things I am not able to appreciate.`,
+    author: `Elbert Hubbard`,
+  },
+  {
+    quote: `I like the dreams of the future better than the history of the past.`,
+    author: `Thomas Jefferson`,
+  },
+  {
+    quote: `Our lives begin to end the day we become silent about things that matter.`,
+    author: `Martin Luther King Jr`,
+  },
+  {
+    quote: `You'll never achieve real success unless you like what you're doing.`,
+    author: `Dale Carnegie`,
+  },
+  {
+    quote: `Success is a matter of sticking to a set of common sense principles anyone can master.`,
+    author: `Earl Nightingale`,
+  },
+  {
+    quote: `Unless you're continually improving your skills, you're quickly becoming irrelevant.`,
+    author: `Stephen Covey`,
+  },
+  {
+    quote: `Success is a lousy teacher. It seduces smart people into thinking they can't lose.`,
+    author: `Bill Gates`,
+  },
+  {
+    quote: `Procrastination is one of the most common and deadliest of diseases and its toll on success and happiness is heavy.`,
+    author: `Wayne Gretzky`,
+  },
+  {
+    quote: `If you work just for money, you'll never make it, but if you love what you're doing and you always put the customer first, success will be yours.`,
+    author: `Ray Kroc`,
+  },
+  {
+    quote: `Your attitude is either the lock on or key to your door of success.`,
+    author: `Denis Waitley`,
+  },
+  {
+    quote: `The successful warrior is the average man, with laser-like focus.`,
+    author: `Bruce Lee`,
+  },
+  {
+    quote: `As I grow older, I pay less attention to what men say. I just watch what they do.`,
+    author: `Andrew Carnegie`,
+  },
+];
+
+export const randNum:number = Math.floor(QUOTES_ARRAY.length * Math.random());
+export const randQuote: String = QUOTES_ARRAY[randNum].quote;
+export const randAuthor:String = QUOTES_ARRAY[randNum].author;
